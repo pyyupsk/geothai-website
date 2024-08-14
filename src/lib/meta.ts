8c6c932cab1749fbf075b358e85cd2ff35a692e1
pyyupsk <pyyupsk@proton.me>
2024-08-14 13:39:38 +0700

    feat(meta): add OpenGraph metadata and common metadata function
    - Added src/lib/meta.ts to generate consistent metadata for pages.
    - Updated src/app/layout.tsx to use dynamic metadata and OpenGraph image.
    - Added public/opengraph.png for OpenGraph image.

---
+++ src/lib/meta.ts
@@ -0,0 +1,27 @@
+import { Metadata } from 'next/dist/lib/metadata/types/metadata-interface'
+import { TemplateString } from 'next/dist/lib/metadata/types/metadata-types'
+
+type CommonMetaData = {
+    title: string | TemplateString
+    description: string
+}
+
+export function commonMetaData({ title, description }: CommonMetaData) {
+    return {
+        title,
+        description,
+        authors: 'GeoThai',
+        metadataBase: new URL('https://geothai.vercel.app/'),
+        openGraph: {
+            title,
+            description,
+            images: [
+                {
+                    url: '/opengraph.png',
+                    width: 1200,
+                    height: 630
+                }
+            ]
+        }
+    } as Metadata
+}
